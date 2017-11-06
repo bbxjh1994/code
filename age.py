9c4ee586686c1c21e979619ce205fb09e18780de
bbxjh1994 <31602212+bbxjh1994@users.noreply.github.com>
2017-11-06 09:12:56 +0800

    Update and rename README.md to age.py

---
+++ age.py
@@ -0,0 +1,10 @@
+# code
+practice about python
+#!/usr/bin/python3
+s = raw_input()
+s = int(s)
+if 1 <= s <= 100:
+    print('your sister is beautiful')
+else:
+    print('your uncle is ugly')
+--------------------------------------------
